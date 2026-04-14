b7ef9a6155d3d3f0ac31e0c40cdb1b240cdfc279
PassiveLemon <lemonl3mn@protonmail.com>
2026-04-14 14:38:08 -0400

    feature: add language servers and linters to home packages
    Dynamically enable the language servers and linters for the languages the user specifies

--- modules/plugins/lsp/default.nix
+++ modules/plugins/lsp/default.nix
@@ -1,32 +1,20 @@
 { config, lib, pkgs, ... }:
 let
-  inherit (lib) mkIf mkOption mkEnableOption types attrNames getAttrs concatMapStrings mapAttrs' nameValuePair mergeAttrsList optionalAttrs length;
+  inherit (lib) mkIf mkOption mkEnableOption types concatMapStrings mapAttrsToList optionalAttrs length flatten optional elem;
   cfg = config.programs.lite-xl;
 
-  supportedServers = import ./servers.nix { inherit lib pkgs; };
-  serverStrings = attrNames supportedServers;
+  serverStrings = [
+    "bashls" "dockerls" "nillsp" "nimlsp" "pyright" "sumneko_lua" "yamlls"
+  ];
 
-  customEnableList = cfg.plugins.lsp.customEnableList;
-
-  # Filter loaded servers
   enableList = cfg.plugins.lsp.enableList;
-  userServers = getAttrs enableList supportedServers;
-  finalServers = mergeAttrsList [ userServers customEnableList ];
-
-  # Map finalServers attrset to xdg.configFile entries
-  # -> {
-  #   "lite-xl/plugins/lsp_servers/lsp_lang1.lua" = { source = "<source1>"; }
-  #   "lite-xl/plugins/lsp_servers/lsp_lang2.lua" = { source = "<source2>"; }
-  #   "lite-xl/plugins/lsp_servers/lsp_lang3.lua" = { source = "<source3>"; }
-  # }
-  namedLspPaths = mapAttrs' (name: source:
-    nameValuePair "lite-xl/plugins/lsp_servers/lsp_${name}.lua" { source = source; })
-    finalServers;
 
   # Concat userLsps list for lua script
   # -> ",serv1,,serv2,,serv3,"
-  finalServerStrings = attrNames finalServers;
-  concatServers = concatMapStrings (serv: ",${serv},") finalServerStrings;
+  concatServers = concatMapStrings (serv: ",${serv},") enableList;
+
+  # Generate a list (nested: [["1"] ["2"] ["3"]]) of packages (language-servers and linters) based on what lsp languages the user specified
+  genPackages = servers: (mapAttrsToList (name: value: optional (elem name enableList) value) servers);
 in
 {
   options = {
@@ -35,33 +23,45 @@ in
         type = types.listOf (types.enum serverStrings);
         default = [ ];
       };
-      customEnableList = mkOption {
-        type = types.attrsOf types.path;
-        default = { };
-      };
-      inheritLanguages = mkEnableOption "inheriting languages for LSP";
+      addPackages = mkEnableOption "adding language servers and linters";
     };
   };
 
   config = mkIf cfg.enable {
-    xdg.configFile = mergeAttrsList [
-      namedLspPaths
-      (optionalAttrs (length finalServerStrings > 0) {
-        # Script to load servers since they are not placed top-level
-        "lite-xl/plugins/lsp_servers/init.lua" = {
-          text = ''
-            -- mod-version: 3
+    xdg.configFile = optionalAttrs (length enableList > 0) {
+      # Script to load servers since they are not placed top-level
+      "lite-xl/plugins/lsp_servers/init.lua" = {
+        text = ''
+          -- mod-version: 3
+          local lspconfig = require "plugins.lsp.config"
+          local servers = '${concatServers}'
 
-            local servers = '${concatServers}'
-
-            -- Match server strings in ",serv1,,serv2,,serv3,"
-            for serv in servers:gmatch(",([%w_]+),") do
-              require("plugins.lsp_servers.lsp_" .. serv)
-            end
-          '';
-        };
-      })
-    ];
+          -- Match server strings in ",serv1,,serv2,,serv3,"
+          for serv in servers:gmatch(",([%w_]+),") do
+            lspconfig[serv].setup()
+          end
+        '';
+      };
+    };
+    # Add language servers and linters dynamically.
+    home.packages = flatten (optional cfg.plugins.lsp.addPackages [
+      (genPackages (with pkgs; {
+        # Language servers
+        "dockerls" = dockerfile-language-server;
+        "sumneko_lua" = lua-language-server;
+        "nilmsp" = nimlsp;
+        "nillsp" = nil;
+        "pyright" = pyright;
+        "bashls" = bash-language-server;
+        "yamlls" = yaml-language-server;
+      }))
+      (genPackages (with pkgs; {
+        # Linters
+        "sumneko_lua" = luajitPackages.luacheck;
+        "pyright" = python312Packages.flake8;
+        "bashls" = shellcheck;
+      }))
+    ]);
   };
 }
 
